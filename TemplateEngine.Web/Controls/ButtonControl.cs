619f77809944ab6699116346b8cc126d9734b499
Marek Fišera <maraf@users.noreply.github.com>
2013-10-07 20:37:01 +0000

    Form base

---
+++ TemplateEngine.Web/Controls/ButtonControl.cs
@@ -0,0 +1,34 @@
+﻿using Neptuo.Templates;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.TemplateEngine.Web.Controls
+{
+    [Html("button")]
+    public class ButtonControl : BaseContentControl
+    {
+        public string Name { get; set; }
+        public string Text { get; set; }
+
+        public ButtonControl(IComponentManager componentManager)
+            : base(componentManager)
+        { }
+
+        public override void OnInit()
+        {
+            base.OnInit();
+
+            if (!String.IsNullOrEmpty(Name))
+                Attributes["name"] = Name;
+
+            if (Content == null)
+            {
+                Content = new List<object>();
+                Content.Add(Text);
+            }
+        }
+    }
+}
---
+++ TemplateEngine.Web/Observers/EventObserver.cs
@@ -0,0 +1,41 @@
+﻿using Neptuo.TemplateEngine.Web.Controls;
+using Neptuo.Templates;
+using Neptuo.Templates.Observers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Neptuo.TemplateEngine.Web.Observers
+{
+    public class EventObserver : IObserver
+    {
+        protected HttpContextBase HttpContext { get; private set; }
+        public string Event { get; set; }
+
+        public EventObserver(HttpContextBase httpContext)
+        {
+            HttpContext = httpContext;
+        }
+
+        public void OnInit(ObserverEventArgs e)
+        {
+            ButtonControl button = e.Target as ButtonControl;
+            if(button == null)
+                throw new InvalidOperationException("EventObserver must used on ButtonControl.");
+
+            if(String.IsNullOrEmpty(button.Name))
+                throw new InvalidOperationException("ButtonControl must have a name.");
+
+            if (HttpContext.Request.Form.AllKeys.Contains(button.Name))
+            {
+
+            }
+        }
+
+        public void Render(ObserverEventArgs e, IHtmlWriter writer)
+        { }
+    }
+}
