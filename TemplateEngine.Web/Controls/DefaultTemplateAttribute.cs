2a38252b680538c3b3e57d2a496c25f2f625a910
Marek Fišera <maraf@users.noreply.github.com>
2013-10-12 13:39:55 +0000

    Preparation for setting properties from url and default template value

---
+++ TemplateEngine.Web/Controls/DefaultTemplateAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.TemplateEngine.Web.Controls
+{
+    [AttributeUsage(AttributeTargets.Property, Inherited = true, AllowMultiple = false)]
+    public class DefaultTemplateAttribute : Attribute
+    {
+        public string Path { get; set; }
+
+        public DefaultTemplateAttribute(string path)
+        {
+            Path = path;
+        }
+    }
+}
