4f2520228f14f63e90894e91dc86dc3aaaee2a87
Marek Fišera <maraf@users.noreply.github.com>
2013-10-13 19:56:33 +0000

    TemplateContent as attributes for UserTemplate

--- TemplateEngine.Web.Compilation/Parsers/UserTemplateComponentBuilder.cs
+++ TemplateEngine.Web.Compilation/Parsers/UserTemplateComponentBuilder.cs
@@ -23,6 +23,44 @@ namespace Neptuo.TemplateEngine.Web.Compilation.Parsers
             TemplatePath = templatePath;
         }
 
+        protected override void AfterBindProperties(IContentBuilderContext context, BindPropertiesContext bindContext, IComponentCodeObject codeObject, IXmlElement element)
+        {
+            base.AfterBindProperties(context, bindContext, codeObject, element);
+
+            if (element.Attributes.Any())
+            {
+                string contentPropertyName = TypeHelper.PropertyName<TemplateControl, object>(t => t.Content);
+                IPropertyDescriptor propertyDescriptor = codeObject.Properties.Where(p => p.Property != null).FirstOrDefault(p => p.Property.Name == contentPropertyName);
+                if (propertyDescriptor == null)
+                {
+                    propertyDescriptor = new ListAddPropertyDescriptor(new TypePropertyInfo(typeof(TemplateControl).GetProperty(contentPropertyName)));
+                    codeObject.Properties.Add(propertyDescriptor);
+                }
+
+                foreach (IXmlAttribute attribute in element.Attributes)
+                {
+                    IComponentCodeObject contentCodeObject = new ComponentCodeObject(typeof(TemplateContentControl));
+                    contentCodeObject.Properties.Add(
+                        new SetPropertyDescriptor(
+                            new TypePropertyInfo(
+                                typeof(TemplateContentControl).GetProperty(TypeHelper.PropertyName<TemplateContentControl, string>(t => t.Name))
+                            ),
+                            new PlainValueCodeObject(attribute.Name)
+                        )
+                    );
+                    contentCodeObject.Properties.Add(
+                        new ListAddPropertyDescriptor(
+                            new TypePropertyInfo(
+                                typeof(TemplateContentControl).GetProperty(TypeHelper.PropertyName<TemplateContentControl, object>(t => t.Content))
+                            ),
+                            new PlainValueCodeObject(attribute.Value)
+                        )
+                    );
+                    propertyDescriptor.SetValue(contentCodeObject);
+                }
+            }
+        }
+
         protected override void BindAttributes(IContentBuilderContext context, BindPropertiesContext bindContext, IComponentCodeObject codeObject, IEnumerable<IXmlAttribute> attributes)
         {
             IPropertyInfo propertyInfo = new TypePropertyInfo(
@@ -42,6 +80,7 @@ namespace Neptuo.TemplateEngine.Web.Compilation.Parsers
                 );
             }
             base.BindAttributes(context, bindContext, codeObject, attributes);
+
         }
     }
 }
