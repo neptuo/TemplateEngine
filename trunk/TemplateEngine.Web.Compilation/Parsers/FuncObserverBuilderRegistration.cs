3a3e19ac8aad5d2a3311b9aa0b76b66b509b93ba
Mara <Mara@a2bec70f-1923-be48-8a98-c51f00bb72a4>
2013-11-15 08:34:13 +0000

    Refactoring observer registration + Default template names includes property name

---
+++ trunk/TemplateEngine.Web.Compilation/Parsers/FuncObserverBuilderRegistration.cs
@@ -0,0 +1,37 @@
+﻿using Neptuo.Templates.Compilation.Parsers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.TemplateEngine.Web.Compilation.Parsers
+{
+    public class FuncObserverBuilderRegistration<T> : IObserverRegistration
+        where T : IObserverBuilder
+    {
+        public Func<T> Factory { get; private set; }
+        public ObserverBuilderScope Scope { get; private set; }
+
+        public FuncObserverBuilderRegistration(Func<T> factory, ObserverBuilderScope scope)
+        {
+            if (factory == null)
+                throw new ArgumentNullException("factory");
+
+            Factory = factory;
+            Scope = scope;
+        }
+
+        public IObserverBuilder CreateBuilder()
+        {
+            return Factory();
+        }
+    }
+
+    public class FuncObserverBuilderRegistration : FuncObserverBuilderRegistration<IObserverBuilder>
+    {
+        public FuncObserverBuilderRegistration(Func<IObserverBuilder> factory, ObserverBuilderScope scope)
+            : base(factory, scope)
+        { }
+    }
+}
