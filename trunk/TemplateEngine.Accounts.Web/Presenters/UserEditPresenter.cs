199912e68e18fa6d5f3e8e32b825508186fa4d51
Mara <Mara@a2bec70f-1923-be48-8a98-c51f00bb72a4>
2013-11-16 21:47:20 +0000

    Refactoring: Clearing user edit presenter

--- trunk/TemplateEngine.Accounts.Web/Presenters/UserEditPresenter.cs
+++ trunk/TemplateEngine.Accounts.Web/Presenters/UserEditPresenter.cs
@@ -23,12 +23,8 @@ namespace Neptuo.TemplateEngine.Accounts.Web.Presenters
     public class UserEditPresenter : PresentationControlBase
     {
         protected IUserAccountRepository UserAccounts { get; private set; }
-        //protected EditUserCommand Model { get; private set; }
-        //protected ICommandDispatcher CommandDispatcher { get; private set; }
-        //protected MessageStorage MessageStorage { get; private set; }
         protected INavigator Navigator { get; private set; }
         protected IModelValueProviderFactory ValueProviderFactory { get; private set; }
-        //protected DataContextStorage DataContext { get; private set; }
         
         [PropertySet(true)]
         public int? UserKey { get; set; }
@@ -36,19 +32,14 @@ namespace Neptuo.TemplateEngine.Accounts.Web.Presenters
         public UserEditPresenter(
             IComponentManager componentManager, 
             PresentationConfiguration<EditUserCommand> configuration, 
-            //ICommandDispatcher commandDispatcher, 
             IUserAccountRepository userAccounts,
-            //MessageStorage messageStorage,
             INavigator navigator
         )
             : base(componentManager, configuration)
         {
             UserAccounts = userAccounts;
-            //CommandDispatcher = commandDispatcher;
-            //MessageStorage = messageStorage;
             Navigator = navigator;
             ValueProviderFactory = configuration.ValueProviderFactory;
-            //DataContext = configuration.DataContext;
             Attributes["method"] = "post";
             Attributes["action"] = "";
         }
@@ -64,48 +55,5 @@ namespace Neptuo.TemplateEngine.Accounts.Web.Presenters
             EditUserCommand model = new EditUserCommand(userAccount);
             return ValueProviderFactory.Create(model);
         }
-
-        //public override void OnInit()
-        //{
-        //    UserAccount userAccount;
-        //    if (UserKey != null)
-        //        userAccount = UserAccounts.Get(UserKey.Value) ?? UserAccounts.Create();
-        //    else
-        //        userAccount = UserAccounts.Create();
-
-        //    Model = new EditUserCommand(userAccount);
-
-        //    IModelValueProvider provider = ValueProviderFactory.Create(Model);
-        //    DataContext.Push(provider);
-
-        //    base.OnInit();
-        //    Init(Template);
-
-        //    DataContext.Pop();
-        //}
-
-        //[Obsolete]
-        //protected void HandleSave()
-        //{
-        //    ModelPresenter.GetData(ValueProviderFactory.Create(Model));
-
-        //    IValidationResult validation = CommandDispatcher.Validate(Model);
-        //    if (validation.IsValid)
-        //    {
-        //        CommandDispatcher.Handle(Model);
-        //        MessageStorage.Add(null, String.Empty, "User account saved.", MessageType.Info);
-        //        Navigator.Open((FormUri)"Accounts.User.List");
-        //    }
-        //    else
-        //    {
-        //        AddModelState(validation);
-        //    }
-        //}
-
-        //protected void AddModelState(IValidationResult validation)
-        //{
-        //    foreach (IValidationMessage message in validation.Messages)
-        //        MessageStorage.Add(null, message.Key, message.Message, MessageType.Error);
-        //}
     }
 }
