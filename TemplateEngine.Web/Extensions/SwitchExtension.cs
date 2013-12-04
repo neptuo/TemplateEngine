a5164c287fe9ec7e042aa0f4ccb1065640700e47
Marek Fišera <maraf@users.noreply.github.com>
2013-12-04 16:50:05 +0000

    SwitchExtension

---
+++ TemplateEngine.Web/Extensions/SwitchExtension.cs
@@ -0,0 +1,41 @@
+﻿using Neptuo.Templates.Extensions;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.TemplateEngine.Web.Extensions
+{
+    [DefaultProperty("Expression")]
+    public class SwitchExtension : IValueExtension
+    {
+        protected IValueConverterService ConverterService { get; private set; }
+        
+        public object Expression { get; set; }
+        public string ConverterKey { get; set; }
+        public object TrueValue { get; set; }
+        public object FalseValue { get; set; }
+
+        public SwitchExtension(IValueConverterService converterService)
+        {
+            ConverterService = converterService;
+        }
+
+        public object ProvideValue(IValueExtensionContext context)
+        {
+            if (!String.IsNullOrEmpty(ConverterKey))
+                Expression = ConverterService.GetConverter(ConverterKey).ConvertTo(Expression);
+
+            bool? value = Expression as bool?;
+            if (value == null)
+                return TrueValue ?? FalseValue;
+
+            if (value.Value)
+                return TrueValue;
+
+            return FalseValue;
+        }
+    }
+}
