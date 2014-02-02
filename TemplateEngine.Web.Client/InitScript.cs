78fd55efda1641c9592060375055e7905689eb2c
Marek Fišera <maraf@users.noreply.github.com>
2014-02-02 11:45:12 +0000

    Mapping javascript views + working ~/Home.aspx

---
+++ TemplateEngine.Web.Client/InitScript.cs
@@ -0,0 +1,47 @@
+﻿using Neptuo.TemplateEngine.PresentationModels;
+using Neptuo.Templates;
+using SharpKit.Html;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.TemplateEngine.Web.Client
+{
+    public static class InitScript
+    {
+        public static void Init(IDependencyContainer container)
+        {
+            container
+                .RegisterType<IStackStorage<IViewStorage>, StackStorage<IViewStorage>>()
+                .RegisterType<IVirtualUrlProvider, UrlProvider>()
+                .RegisterType<ICurrentUrlProvider, UrlProvider>()
+                .RegisterType<IParameterProviderFactory, ParameterProviderFactory>()
+                .RegisterInstance(new TemplateContentStorageStack());
+        }
+    }
+
+    public class UrlProvider : IVirtualUrlProvider, ICurrentUrlProvider
+    {
+        public string ResolveUrl(string path)
+        {
+            return path.Replace("~/", "/");
+        }
+
+        public string GetCurrentUrl()
+        {
+            return HtmlContext.location.href;
+        }
+    }
+
+    public class ParameterProviderFactory : IParameterProviderFactory
+    {
+        public IParameterProvider Provider(ParameterProviderType providerType)
+        {
+            return null;
+        }
+    }
+
+
+}
