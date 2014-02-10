5cdc08b560738ab050652656057e5873837e8090
Mara <Mara@a2bec70f-1923-be48-8a98-c51f00bb72a4>
2014-02-10 14:03:51 +0000

    Basic UserRepository for js

---
+++ trunk/TemplateEngine.Accounts.Client/Web/DataSources/UserAccountEditDataSource.cs
@@ -0,0 +1,35 @@
+﻿using Neptuo.TemplateEngine.Accounts.Data;
+using Neptuo.TemplateEngine.Web.DataSources;
+using SharpKit.Html;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.TemplateEngine.Accounts.Web.DataSources
+{
+    public class UserAccountEditDataSource : IDataSource
+    {
+        private UserRepository userAccounts;
+
+        public int Key { get; set; }
+
+        public UserAccountEditDataSource(UserRepository userAccounts)
+        {
+            Guard.NotNull(userAccounts, "userAccounts");
+            this.userAccounts = userAccounts;
+        }
+
+        public void GetItem(Action<object> callback)
+        {
+            HtmlContext.setTimeout(() =>
+            {
+                if (Key > 0)
+                    callback(userAccounts.GetAll().FirstOrDefault(u => u.Key == Key));
+                else
+                    callback(new UserAccountEditModel { Key = 0, IsEnabled = true });
+            }, 1000);
+        }
+    }
+}
