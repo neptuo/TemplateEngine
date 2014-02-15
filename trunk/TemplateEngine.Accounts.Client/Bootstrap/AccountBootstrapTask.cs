9f774f4aee83ca26e1a3f9cc2006e023640cca2f
Mara <Mara@a2bec70f-1923-be48-8a98-c51f00bb72a4>
2014-02-15 16:49:09 +0000

    Preparing client controllers

--- trunk/TemplateEngine.Accounts.Client/Bootstrap/AccountBootstrapTask.cs
+++ trunk/TemplateEngine.Accounts.Client/Bootstrap/AccountBootstrapTask.cs
@@ -1,5 +1,9 @@
 ﻿using Neptuo.Bootstrap;
 using Neptuo.TemplateEngine.Accounts.Data;
+using Neptuo.TemplateEngine.Accounts.Web.Controllers;
+using Neptuo.TemplateEngine.Navigation.Bootstrap;
+using Neptuo.TemplateEngine.Web;
+using Neptuo.TemplateEngine.Web.Controllers;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,20 +12,37 @@ using System.Threading.Tasks;
 
 namespace Neptuo.TemplateEngine.Accounts.Bootstrap
 {
-    public class AccountBootstrapTask : IBootstrapTask
+    public class AccountBootstrapTask : AccountBootstrapTaskBase, IBootstrapTask
     {
-        private IDependencyContainer container;
+        private IDependencyContainer dependencyContainer;
+        private IFormUriRegistry formRegistry;
+        private IControllerRegistry controllerRegistry;
+        private GlobalNavigationCollection globalNavigations;
 
-        public AccountBootstrapTask(IDependencyContainer container)
+        public AccountBootstrapTask(IDependencyContainer dependencyContainer, IFormUriRegistry formRegistry, IControllerRegistry controllerRegistry, GlobalNavigationCollection globalNavigations)
         {
-            Guard.NotNull(container, "container");
-            this.container = container;
+            Guard.NotNull(dependencyContainer, "dependencyContainer");
+            Guard.NotNull(formRegistry, "formRegistry");
+            Guard.NotNull(controllerRegistry, "controllerRegistry");
+            Guard.NotNull(globalNavigations, "globalNavigations");
+
+            this.dependencyContainer = dependencyContainer;
+            this.formRegistry = formRegistry;
+            this.controllerRegistry = controllerRegistry;
+            this.globalNavigations = globalNavigations;
         }
 
         public void Initialize()
         {
-            container
+            dependencyContainer
                 .RegisterInstance(new UserRepository());
+
+            controllerRegistry
+                .Add("Accounts/User/Create", new DependencyControllerFactory(dependencyContainer, typeof(UserAccountController)));
+                //.Add(dependencyContainer, typeof(UserAccountController));
+
+            RegisterForms(formRegistry);
+            RegisterGlobalNavigations(globalNavigations);
         }
     }
 }
---
+++ trunk/TemplateEngine.Accounts.Client/Web/Controllers/UserAccountController.cs
@@ -0,0 +1,29 @@
+﻿using Neptuo.TemplateEngine.Web.Controllers;
+using Neptuo.TemplateEngine.Web.Controllers.Binders;
+using SharpKit.Html;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.TemplateEngine.Accounts.Web.Controllers
+{
+    public class UserAccountController : IController
+    {
+        public IControllerContext Context { get; private set; }
+
+        [Action("Accounts/User/Create")]
+        public void Create()
+        {
+            EditUserCommand model = Context.ModelBinder.Bind<EditUserCommand>(new EditUserCommand());
+            HtmlContext.alert(model);
+        }
+
+        public void Execute(IControllerContext context)
+        {
+            Context = context;
+            Create();
+        }
+    }
+}
