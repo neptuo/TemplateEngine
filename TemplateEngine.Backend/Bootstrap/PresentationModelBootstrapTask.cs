923dce44873be75278e6f80e520d22941f588c13
Marek Fišera <maraf@users.noreply.github.com>
2014-02-27 22:04:33 +0000

    Refactoring Backend

--- TemplateEngine.Backend/Bootstrap/PresentationModelBootstrapTask.cs
+++ TemplateEngine.Backend/Bootstrap/PresentationModelBootstrapTask.cs
@@ -11,7 +11,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Neptuo.TemplateEngine.Backend
+namespace Neptuo.TemplateEngine.Backend.Bootstrap
 {
     public class PresentationModelBootstrapTask : IBootstrapTask
     {
