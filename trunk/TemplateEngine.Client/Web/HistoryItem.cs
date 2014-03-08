9bbf96666287ab865014cdfdc96fb64afb7ebc74
Mara <Mara@a2bec70f-1923-be48-8a98-c51f00bb72a4>
2014-03-08 10:01:52 +0000

    Progress on client state management

---
+++ trunk/TemplateEngine.Client/Web/HistoryItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.TemplateEngine.Web
+{
+    public class HistoryItem
+    {
+        public string Url { get; private set; }
+        public FormRequestContext FormData { get; private set; }
+
+        public HistoryItem(string url, FormRequestContext formData)
+        {
+            Url = url;
+            FormData = formData;
+        }
+    }
+}
