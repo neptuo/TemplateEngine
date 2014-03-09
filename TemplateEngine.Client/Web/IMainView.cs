315b2d3c6f4ec8e4161bccdb6aa5524ded137119
Marek Fišera <maraf@users.noreply.github.com>
2014-03-09 15:12:50 +0000

    Added comments

--- TemplateEngine.Client/Web/IMainView.cs
+++ TemplateEngine.Client/Web/IMainView.cs
@@ -7,13 +7,40 @@ using System.Threading.Tasks;
 
 namespace Neptuo.TemplateEngine.Web
 {
+    /// <summary>
+    /// Represents whole user interface.
+    /// </summary>
     public interface IMainView
     {
+        /// <summary>
+        /// Invoked when user clicks a link.
+        /// </summary>
         event Action<string, string[]> OnLinkClick;
+
+        /// <summary>
+        /// Invoked when user posts a form.
+        /// </summary>
         event Action<FormRequestContext> OnPostFormSubmit;
+        
+        /// <summary>
+        /// Invoked when user submits GET form.
+        /// </summary>
         event Action<string, string[]> OnGetFormSubmit;
 
+        /// <summary>
+        /// Renders user inteface with <paramref name="viewPath"/> and updates <paramref name="toUpdate"/> parts of view.
+        /// Uses <paramref name="dependencyContainer"/> as dependencyContainer for rendering view.
+        /// </summary>
+        /// <param name="viewPath">New view.</param>
+        /// <param name="toUpdate">Parts of user interface to update.</param>
+        /// <param name="dependencyContainer">Depdency container used to render view.</param>
         void RenderView(string viewPath, string[] toUpdate, IDependencyContainer dependencyContainer);
+
+        /// <summary>
+        /// Updates content of element with <paramref name="partialGuid"/> with content from <paramref name="content"/>.
+        /// </summary>
+        /// <param name="partialGuid">Guid of element to update.</param>
+        /// <param name="content">New content for element with <paramref name="partialGuid"/>.</param>
         void UpdateView(string partialGuid, TextWriter content);
     }
 }
