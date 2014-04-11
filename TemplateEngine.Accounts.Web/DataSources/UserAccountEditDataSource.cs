2c3c80d82b162191c3414401804dfc6833b8d1d6
Marek Fišera <maraf@users.noreply.github.com>
2014-04-11 11:16:42 +0000

    Assigning roles to accounts doesn't work correctly

--- TemplateEngine.Accounts.Web/DataSources/UserAccountEditDataSource.cs
+++ TemplateEngine.Accounts.Web/DataSources/UserAccountEditDataSource.cs
@@ -2,7 +2,7 @@
 using Neptuo.Data.Queries;
 using Neptuo.PresentationModels.TypeModels;
 using Neptuo.TemplateEngine.Accounts.Commands;
-using Neptuo.TemplateEngine.Accounts.Data.Queries;
+using Neptuo.TemplateEngine.Accounts.Data;
 using Neptuo.TemplateEngine.Accounts.Queries;
 using Neptuo.TemplateEngine.Web;
 using Neptuo.TemplateEngine.Web.Controllers.Binders;
@@ -18,28 +18,30 @@ namespace Neptuo.TemplateEngine.Accounts.Web.DataSources
     [WebDataSource]
     public class UserAccountEditDataSource : IDataSource
     {
-        private IUserAccountQuery userQuery;
-        private IActivator<UserAccount> userFactory;
-        private IModelValueProviderFactory providerFactory;
+        private UserAccountDataService dataService;
         private IModelBinder modelBinder;
 
         public int Key { get; set; }
 
-        public UserAccountEditDataSource(IUserAccountQuery userQuery, IActivator<UserAccount> userFactory, IModelValueProviderFactory providerFactory, IModelBinder modelBinder)
+        public UserAccountEditDataSource(UserAccountDataService dataService, IModelBinder modelBinder)
         {
-            this.userQuery = userQuery;
-            this.userFactory = userFactory;
-            this.providerFactory = providerFactory;
+            Guard.NotNull(dataService, "accountDataService");
+            Guard.NotNull(modelBinder, "modelBinder");
+            this.dataService = dataService;
             this.modelBinder = modelBinder;
         }
 
         public object GetItem()
         {
-            userQuery.Filter.Key = IntSearch.Create(Key);
+            dataService.Query.Filter.Key = IntSearch.Create(Key);
 
-            UserAccountEditModel model = MapEntityToModel(userQuery.ResultSingle() ?? userFactory.Create());
-            model = modelBinder.Bind<UserAccountEditModel>(model);
-            return model;
+            UserAccount account1 = dataService.Query.Result().Items.ToList().FirstOrDefault();
+            UserAccount account2 = dataService.Repository.Get(Key);
+
+            UserAccountEditModel model1 = MapEntityToModel(account1 ?? dataService.Factory.Create());
+            UserAccountEditModel model2 = MapEntityToModel(account2 ?? dataService.Factory.Create());
+            model1 = modelBinder.Bind<UserAccountEditModel>(model1);
+            return model1;
             //return providerFactory.Create(model);
         }
 
