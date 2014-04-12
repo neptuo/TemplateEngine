b8f14bd00581317b7c15ff2d8a22d2d549bb1c2e
Mara <Mara@a2bec70f-1923-be48-8a98-c51f00bb72a4>
2014-04-12 07:33:50 +0000

    Positibility for client controls to process async operations

---
+++ trunk/TemplateEngine.Client/Web/AsyncNotifyService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.TemplateEngine.Web
+{
+    public class AsyncNotifyService
+    {
+        private int counter = 0;
+        private HashSet<object> sources = new HashSet<object>();
+
+        public event Action OnReady;
+        
+        public bool IsReady
+        {
+            get { return counter == 0; }
+        }
+
+        public void Register(object source)
+        {
+            if (sources.Add(source))
+                counter++;
+        }
+
+        public void NotifyDone(object source)
+        {
+            if (sources.Remove(sources))
+            {
+                counter--;
+
+                if (OnReady != null)
+                    OnReady();
+            }
+        }
+    }
+}
