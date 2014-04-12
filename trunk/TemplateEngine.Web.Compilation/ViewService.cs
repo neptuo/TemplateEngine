ac8e634bc24e409b226e4a98829eae8eab0cb11b
Mara <Mara@a2bec70f-1923-be48-8a98-c51f00bb72a4>
2014-04-12 12:40:45 +0000

    Fixed default templates for javascript source code

--- trunk/TemplateEngine.Web.Compilation/ViewService.cs
+++ trunk/TemplateEngine.Web.Compilation/ViewService.cs
@@ -3,6 +3,7 @@ using Neptuo.Templates;
 using Neptuo.Templates.Compilation;
 using Neptuo.Templates.Compilation.CodeGenerators;
 using Neptuo.Templates.Compilation.CodeObjects;
+using Neptuo.Templates.Compilation.PreProcessing;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -17,6 +18,8 @@ namespace Neptuo.TemplateEngine.Web.Compilation
         public string GenerateJavascriptSourceCodeFromView(string viewContent, IViewServiceContext context, INaming naming)
         {
             IPropertyDescriptor propertyDescriptor = ParseViewContent(viewContent, context);
+            PreProcessorService.Process(propertyDescriptor, new DefaultPreProcessorServiceContext(context.DependencyProvider));
+
             return GenerateJavascriptSourceCode(propertyDescriptor, context, naming);
         }
     }
