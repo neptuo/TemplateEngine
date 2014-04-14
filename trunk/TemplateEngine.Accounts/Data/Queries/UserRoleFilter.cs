4214aed9b37edc69066def4b1f614d058a614a66
Mara <Mara@a2bec70f-1923-be48-8a98-c51f00bb72a4>
2014-04-14 21:25:35 +0000

    Removed Accounts.Data, moved to BL

---
+++ trunk/TemplateEngine.Accounts/Data/Queries/UserRoleFilter.cs
@@ -0,0 +1,16 @@
+﻿using Neptuo.Data.Queries;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.TemplateEngine.Accounts.Data.Queries
+{
+    public class UserRoleFilter : IUserRoleFilter
+    {
+        public IntSearch Key { get; set; }
+        public TextSearch Name { get; set; }
+        public TextSearch Description { get; set; }
+    }
+}
