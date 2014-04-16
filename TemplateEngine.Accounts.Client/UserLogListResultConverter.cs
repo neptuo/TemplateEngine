8a8703696b08fa58a8ed56cff5038f096e42d545
Marek Fišera <maraf@users.noreply.github.com>
2014-04-16 07:38:49 +0000

    Base for user log view (added sources)

---
+++ TemplateEngine.Accounts.Client/UserLogListResultConverter.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Neptuo.ComponentModel.Converters;
+using SharpKit.JavaScript;
+
+namespace Neptuo.TemplateEngine.Accounts
+{
+    public class UserLogListResultConverter : ConverterBase<JsObject, UserLogListResult>
+    {
+        public override bool TryConvert(JsObject sourceValue, out UserLogListResult targetValue)
+        {
+            int totalCount = sourceValue["TotalCount"].As<int>();
+            JsObject data = sourceValue["Data"].As<JsObject>();
+
+            targetValue = new UserLogListResult(GetLogs(data), totalCount);
+            return true;
+        }
+
+        private IEnumerable<UserLogViewModel> GetLogs(JsObject sourceValue)
+        {
+            List<UserLogViewModel> result = new List<UserLogViewModel>();
+
+            JsArray array = sourceValue.As<JsArray>();
+            for (int i = 0; i < array.length; i++)
+            {
+                JsObject item = array[i].As<JsObject>();
+                result.Add(GetLog(item));
+            }
+
+            return result;
+        }
+
+        private UserLogViewModel GetLog(JsObject sourceValue)
+        {
+            return new UserLogViewModel(
+                sourceValue["Key"].As<string>(),
+                GetAccount(sourceValue["User"].As<JsObject>()),
+                sourceValue["SignedIn"].As<DateTime>(),
+                sourceValue["SignedOut"].As<DateTime?>(),
+                sourceValue["LastActivity"].As<DateTime?>(),
+                sourceValue["IsCurrent"].As<bool>()
+            );
+        }
+
+        private UserAccountRowViewModel GetAccount(JsObject sourceValue)
+        {
+            return new UserAccountRowViewModel(sourceValue["Key"].As<int>(), sourceValue["Username"].As<string>());
+        }
+    }
+}
---
+++ TemplateEngine.Accounts.Shared/UserLogViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.TemplateEngine.Accounts
+{
+    public class UserLogViewModel
+    {
+        public string Key { get; set; }
+        public UserAccountRowViewModel User { get; set; }
+        public DateTime SignedIn { get; set; }
+        public DateTime? SignedOut { get; set; }
+        public DateTime? LastActivity { get; set; }
+        public bool IsCurrent { get; set; }
+
+        public UserLogViewModel(string key, UserAccountRowViewModel user, DateTime signedIn, DateTime? signedOut, DateTime? lastActivity, bool isCurrent = false)
+        {
+            Key = key;
+            User = user;
+            SignedIn = signedIn;
+            SignedOut = signedOut;
+            LastActivity = lastActivity;
+            IsCurrent = isCurrent;
+        }
+    }
+}
