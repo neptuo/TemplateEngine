59c004e5328e723841486a8ffc389dd20138243b
Mara <Mara@a2bec70f-1923-be48-8a98-c51f00bb72a4>
2014-04-27 21:05:38 +0000

    Base for HostedApplication and IApplicationBuilder

---
+++ trunk/TemplateEngine.Hosting/Bootstrap/HostedApplication.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Neptuo.TemplateEngine.Hosting.Bootstrap
+{
+    public class HostedApplication : HttpApplication
+    {
+        private IApplicationBuilder builder;
+
+        public HostedApplication(IApplicationBuilder builder)
+        {
+            Guard.NotNull(builder, "builder");
+            this.builder = builder;
+        }
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            OnStart();
+        }
+
+        protected virtual void OnStart()
+        { }
+    }
+}
