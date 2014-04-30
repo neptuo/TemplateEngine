e8375cc4e44f6e2bb6c9bad33130b4a3853820ed
Mara <Mara@a2bec70f-1923-be48-8a98-c51f00bb72a4>
2014-04-30 07:39:46 +0000

    TemplateEngine.Hosting.Integration project for integration to webapp

--- trunk/TemplateEngine.Hosting.Integration/HostedApplication.cs
+++ trunk/TemplateEngine.Hosting.Integration/HostedApplication.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Web;
 
-namespace Neptuo.TemplateEngine.Hosting.Bootstrap
+namespace Neptuo.TemplateEngine.Hosting.Integration
 {
     public class HostedApplication : HttpApplication
     {
