d81d56c07957f61a1b56cfe0cb482ca4ba2da867
Mara <Mara@a2bec70f-1923-be48-8a98-c51f00bb72a4>
2014-05-01 15:50:51 +0000

    Base for Publishing

---
+++ trunk/TemplateEngine.Publishing.Data.Entity/EntityArticleRepository.cs
@@ -0,0 +1,21 @@
+﻿using Neptuo.Data.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.TemplateEngine.Publishing.Data.Entity
+{
+    public class EntityArticleRepository : EntityRepository<Article, DataContext>, IArticleRepository
+    {
+        public EntityArticleRepository(DataContext dbContext)
+            : base(dbContext)
+        { }
+
+        public Article Create()
+        {
+            return DbContext.Articles.Create();
+        }
+    }
+}
---
+++ trunk/TemplateEngine.Publishing/Data/Queries/IArticleLineFilter.cs
@@ -0,0 +1,16 @@
+﻿using Neptuo.Data.Queries;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neptuo.TemplateEngine.Publishing.Data.Queries
+{
+    public interface IArticleLineFilter
+    {
+        IntSearch Key { get; set; }
+        TextSearch Name { get; set; }
+        TextSearch Url { get; set; }
+    }
+}
