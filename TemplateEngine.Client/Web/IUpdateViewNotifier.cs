708761747832a6b2edb632171047d8580cd6eeaa
Marek Fišera <maraf@users.noreply.github.com>
2014-05-16 06:53:42 +0000

    Updated comments

--- TemplateEngine.Client/Web/IUpdateViewNotifier.cs
+++ TemplateEngine.Client/Web/IUpdateViewNotifier.cs
@@ -6,7 +6,7 @@ using System.Text;
 namespace Neptuo.TemplateEngine.Web
 {
     /// <summary>
-    /// Helps maintaing "Loading" view.
+    /// Helps maintaining "Loading" state.
     /// </summary>
     public interface IUpdateViewNotifier
     {
